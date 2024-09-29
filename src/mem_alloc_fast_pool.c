b0bed2a5bebe76301c21ae462b436d731d5d584d
hiba-aaboud <hibamaroua31@gmail.com>
2024-09-29 17:53:53 +0200

    Initial commit

---
+++ src/mem_alloc_fast_pool.c
@@ -0,0 +1,81 @@
+#include <assert.h>
+#include <stdio.h>
+#include "mem_alloc_fast_pool.h"
+#include "my_mmap.h"
+#include "mem_alloc.h"
+
+void init_fast_pool(mem_pool_t *p, size_t size, size_t min_request_size, size_t max_request_size)
+{
+    /* TO BE IMPLEMENTED */
+    p->start_addr = my_mmap(size);
+
+    //  free list
+    p->end_addr = (void *)((char *)p->start_addr + size);
+    p->first_free = p->start_addr;
+    p->min_req_size = min_request_size;
+    p->max_req_size = max_request_size;
+
+    mem_fast_free_block_t *current_block = (mem_fast_free_block_t *)p->start_addr;
+    mem_fast_free_block_t *next_block = NULL;
+
+    while ((void *)((char *)current_block + max_request_size) < p->end_addr)
+    {
+        next_block = (mem_fast_free_block_t *)((char *)current_block + max_request_size);
+        current_block->next = next_block;
+        current_block = next_block;
+    }
+    current_block->next = NULL;
+
+    printf("%s:%d:Init implemented!\n", __FUNCTION__, __LINE__);
+}
+
+void *mem_alloc_fast_pool(mem_pool_t *pool, size_t size)
+{
+    /* TO BE IMPLEMENTED */
+    if (size > pool->max_req_size || size < pool->min_req_size)
+    {
+        exit(0);
+    }
+
+    if (pool->first_free == NULL)
+    {
+        printf("no mem");
+        return NULL;
+    }
+
+    void *allocated_block = pool->first_free;
+    mem_fast_free_block_t *first_b = (mem_fast_free_block_t*) pool->first_free;
+    pool->first_free = first_b->next; 
+
+    printf("%s:%d: Alloc implemented!\n", __FUNCTION__, __LINE__);
+
+    return allocated_block;
+}
+
+void mem_free_fast_pool(mem_pool_t *pool, void *b)
+{
+    /* TO BE IMPLEMENTED */
+
+    // printf("%s:%d: Please, implement me!\n", __FUNCTION__, __LINE__);
+    // *(void **)b = pool->first_free;
+    // pool->first_free = b;
+
+    if (b == NULL) {
+        return; 
+    }
+
+    mem_fast_free_block_t *freed_block = (mem_fast_free_block_t *)b;
+    freed_block->next = pool->first_free;  
+    pool->first_free = freed_block;   
+
+}
+
+size_t mem_get_allocated_block_size_fast_pool(mem_pool_t *pool, void *addr)
+{
+    size_t res;
+    res = pool->max_req_size;
+    return res;
+}
+
+
+
---
+++ src/mem_alloc_standard_pool.c
@@ -0,0 +1,217 @@
+#include <stdlib.h>
+#include <assert.h>
+#include <stdio.h>
+
+#include "mem_alloc_types.h"
+#include "mem_alloc_standard_pool.h"
+#include "my_mmap.h"
+#include "mem_alloc.h"
+
+/////////////////////////////////////////////////////////////////////////////
+
+#ifdef STDPOOL_POLICY
+/* Get the value provided by the Makefile */
+std_pool_placement_policy_t std_pool_policy = STDPOOL_POLICY;
+#else
+std_pool_placement_policy_t std_pool_policy = DEFAULT_STDPOOL_POLICY;
+#endif
+
+/////////////////////////////////////////////////////////////////////////////
+
+void init_standard_pool(mem_pool_t *p, size_t size, size_t min_request_size, size_t max_request_size)
+{    /* TO BE IMPLEMENTED */
+    p->start_addr = my_mmap(size);
+    p->end_addr = (void *)((char *)p->start_addr + size);
+
+    // Initialize the first free block to cover the entire pool
+    mem_std_free_block_t *initial_free_block = (mem_std_free_block_t *)p->start_addr;
+    set_block_size(&initial_free_block->header, size - 2 * sizeof(mem_std_block_header_footer_t));
+    set_block_free(&initial_free_block->header);
+    
+    // Set footer for the initial free block
+    mem_std_block_header_footer_t *footer = (mem_std_block_header_footer_t *)((char *)initial_free_block + get_block_size(&initial_free_block->header) + sizeof(mem_std_block_header_footer_t));
+    *footer = initial_free_block->header;
+    
+    initial_free_block->prev = NULL;
+    initial_free_block->next = NULL;
+    p->first_free = initial_free_block;
+    printf("%s:%d: Please, implement me!\n", __FUNCTION__, __LINE__);
+}
+
+
+//question 6
+static void split_free_block(mem_std_free_block_t *block, size_t size) {
+    size_t block_size = get_block_size(&block->header);
+    size_t sizeRestante = block_size - size - 2 * sizeof(mem_std_block_header_footer_t);
+
+    if (sizeRestante >= sizeof(mem_std_free_block_t)) {
+        //new block
+        mem_std_free_block_t *new_block = (mem_std_free_block_t *)((char *)block + size + 2 * sizeof(mem_std_block_header_footer_t));
+        set_block_size(&new_block->header, sizeRestante);
+        set_block_free(&new_block->header);
+
+        mem_std_block_header_footer_t *new_footer = (mem_std_block_header_footer_t *)((char *)new_block + sizeRestante + sizeof(mem_std_block_header_footer_t));
+        *new_footer = new_block->header;
+    //the free list
+        new_block->next = block->next;
+        new_block->prev = block;
+        if (block->next) {
+            block->next->prev = new_block;
+        }
+        block->next = new_block;
+
+        set_block_size(&block->header, size);
+    }
+}
+
+void *mem_alloc_standard_pool(mem_pool_t *pool, size_t size)
+{      /* TO BE IMPLEMENTED */
+    printf("%s:%d: Please, implement me!\n", __FUNCTION__, __LINE__);
+
+    // if (size % MEM_ALIGN != 0) {
+    // size = ((size / MEM_ALIGN) + 1) * MEM_ALIGN;
+    // }
+    size_t req_size = size + 2 * sizeof(mem_std_block_header_footer_t);
+
+    mem_std_free_block_t *current = (mem_std_free_block_t *)pool->first_free;
+    while (current != NULL) {
+        if (is_block_free(&current->header) && get_block_size(&current->header) >= req_size) {
+            split_free_block(current, size);
+            set_block_used(&current->header);
+             if (current->prev) {
+                current->prev->next = current->next;
+            } else {
+                pool->first_free = current->next;
+            }
+            if (current->next) {
+                current->next->prev = current->prev;
+            }
+
+         return (char *)current + sizeof(mem_std_block_header_footer_t);
+        }
+        current = current->next;
+    }
+    return NULL; // No block found
+    
+}
+//question 7
+static void coalesce_blocks(mem_pool_t *pool, mem_std_free_block_t *block) {
+    // Coalesce with the next block if it's free and adjacent
+    mem_std_free_block_t *next_block = (mem_std_free_block_t *)((char *)block + get_block_size(&block->header) + 2 * sizeof(mem_std_block_header_footer_t));
+    if ((char *)next_block < (char *)pool->end_addr && is_block_free(&next_block->header)) {
+        // Combine the sizes
+        size_t new_size = get_block_size(&block->header) + get_block_size(&next_block->header) + 2 * sizeof(mem_std_block_header_footer_t);
+        set_block_size(&block->header, new_size);
+        set_block_free(&block->header);
+        mem_std_block_header_footer_t *footer = (mem_std_block_header_footer_t *)((char *)block + new_size + sizeof(mem_std_block_header_footer_t));
+        *footer = block->header;
+        block->next = next_block->next;
+        if (next_block->next) {
+            next_block->next->prev = block;
+        }
+    }
+    mem_std_free_block_t *prev_block = block->prev;
+    if (prev_block && is_block_free(&prev_block->header)) {
+        size_t new_size = get_block_size(&prev_block->header) + get_block_size(&block->header) + 2 * sizeof(mem_std_block_header_footer_t);
+        set_block_size(&prev_block->header, new_size);
+        set_block_free(&prev_block->header);
+        mem_std_block_header_footer_t *footer = (mem_std_block_header_footer_t *)((char *)prev_block + new_size + sizeof(mem_std_block_header_footer_t));
+        *footer = prev_block->header;
+
+        // Update the free list pointers
+        prev_block->next = block->next;
+        if (block->next) {
+            block->next->prev = prev_block;
+        }
+        block = prev_block;
+    }
+}
+
+//  void mem_free_standard_pool(mem_pool_t *pool, void *addr)
+// {
+//     /* TO BE IMPLEMENTED */
+//     printf("%s:%d: Please, implement me!\n", __FUNCTION__, __LINE__);
+//  if (addr == NULL) return;
+
+//     // Get the block header from the pointer
+//     mem_std_free_block_t *block = (mem_std_free_block_t *)((char *)addr - sizeof(mem_std_block_header_footer_t));
+//     set_block_free(&block->header);
+
+//     // Set footer for the block
+//     mem_std_block_header_footer_t *footer = (mem_std_block_header_footer_t *)((char *)block + get_block_size(&block->header) + sizeof(mem_std_block_header_footer_t));
+//     *footer = block->header;
+
+//     // Coalesce with adjacent blocks
+//     coalesce_blocks(pool, block);
+
+//     // Insert into the free list in the correct position
+//     mem_std_free_block_t *current = (mem_std_free_block_t *)pool->first_free;
+//     mem_std_free_block_t *prev = NULL;
+//     while (current != NULL && (char *)current < (char *)block) {
+//         prev = current;
+//         current = current->next;
+//     }
+
+//     block->next = current;
+//     block->prev = prev;
+//     if (current) {
+//         current->prev = block;
+//     }
+//     if (prev) {
+//         prev->next = block;
+//     } else {
+//         pool->first_free = block;
+//     }
+
+// }
+
+void mem_free_standard_pool(mem_pool_t *pool, void *addr)
+{/* TO BE IMPLEMENTED */
+    printf("%s:%d: Please, implement me!\n", __FUNCTION__, __LINE__);
+    if (addr == NULL) return;
+
+
+    mem_std_free_block_t *block = (mem_std_free_block_t *)((char *)addr - sizeof(mem_std_block_header_footer_t));
+    set_block_free(&block->header);
+    mem_std_block_header_footer_t *footer = (mem_std_block_header_footer_t *)((char *)block + get_block_size(&block->header) + sizeof(mem_std_block_header_footer_t));
+    *footer = block->header;
+
+    coalesce_blocks(pool, block);
+
+    mem_std_free_block_t *current = (mem_std_free_block_t *)pool->first_free;
+    mem_std_free_block_t *prev = NULL;
+
+
+    while (current != NULL && (char *)current < (char *)block) {
+        prev = current;
+        current = current->next;
+    }
+
+ 
+    block->next = current;
+    block->prev = prev;
+    
+    if (current != NULL) {
+        current->prev = block;
+    }
+    
+    if (prev != NULL) {
+        prev->next = block;
+    } else {
+        pool->first_free = block;  
+    }
+
+
+    printf("addres dyal free block: %p, size: %zu, \n", block, get_block_size(&block->header));
+}
+
+size_t mem_get_allocated_block_size_standard_pool(mem_pool_t *pool, void *addr)
+{
+    /* TO BE IMPLEMENTED */
+    printf("%s:%d: Please, implement me!\n", __FUNCTION__, __LINE__);
+    if (addr == NULL) return 0;
+
+    // Get the block header from the pointer
+    mem_std_block_header_footer_t *header = (mem_std_block_header_footer_t *)((char *)addr - sizeof(mem_std_block_header_footer_t));
+    return get_block_size(header);
+}
